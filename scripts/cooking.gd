4af09fb02e85dafc14e1ebd5e730df0f685ea23d
Emily <emilyahmad77@gmail.com>
2026-06-21 20:30:16 -0400

    Added more models

--- scripts/cooking.gd
+++ scripts/cooking.gd
@@ -1,19 +1,23 @@
 extends Node3D
 
-var open_window_pos = Vector3(-1.99298, 2.85601, -0.391589)
-var closed_window_pos = Vector3(-1.99298, 1.08476, -0.391589)
+var open_window_pos = Vector3(-2.046472, 4.24953, 0.722781)
+var closed_window_pos = Vector3(-2.046472, 1.845929, 0.722781)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#print($Window.global_position)
 	$Window.global_position = open_window_pos
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	if Input.is_action_just_pressed("close_window"):
-		print("closing window..")
 		$Window.global_position = closed_window_pos
 		
 	if Input.is_action_just_pressed("open_window"):
-		print("opening window..")
 		$Window.global_position = open_window_pos
+
+
+func _on_bowl_reference_body_exited(body: Node) -> void:
+	print("collided with ", body.name)
