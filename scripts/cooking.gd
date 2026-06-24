d3ff75ba0697ee5e66ac919732a743e151b29fc4
Emily <emilyahmad77@gmail.com>
2026-06-23 22:50:00 -0400

    Detects mixer/bowl placement

--- scripts/cooking.gd
+++ scripts/cooking.gd
@@ -3,11 +3,13 @@ extends Node3D
 var open_window_pos = Vector3(-2.046472, 4.24953, 0.722781)
 var closed_window_pos = Vector3(-2.046472, 1.845929, 0.722781)
 
+var bowl_placed := false
+var mixer_placed := false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#print($Window.global_position)
 	$Window.global_position = open_window_pos
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -18,6 +20,17 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("open_window"):
 		$Window.global_position = open_window_pos
 
+func _on_bowl_body_entered(body: Node) -> void:
+	if body.name == "PlacematBody":
+		bowl_placed = true
+		_check_both_placed()
 
-func _on_bowl_reference_body_exited(body: Node) -> void:
-	print("collided with ", body.name)
+func _on_mixer_body_entered(body: Node) -> void:
+	if body.name == "PlacematBody":
+		mixer_placed = true
+		_check_both_placed()
+
+func _check_both_placed() -> void:
+	if bowl_placed and mixer_placed:
+		var both_placed = true
+		print("bowl and mixer are placed!")
